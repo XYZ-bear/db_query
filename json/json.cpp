4f7e011ff1fbcff7399fa4d84159ed80e80470aa
xyz <xiongyan.zhong@foxmail.com>
2020-08-07 19:43:03 +0800

    json to C++

--- json/json.cpp
+++ json/json.cpp
@@ -15,6 +15,7 @@ int for_key_value(const char* begin);
 int for_array(const char* begin);
 
 int for_str(const char* begin) {
+	//cout << "len: " << strlen(begin) << endl;
 	int index = 0;
 	bool start = false;
 	while (char ch = *(begin + index)) {
@@ -25,11 +26,13 @@ int for_str(const char* begin) {
 		}
 		index++;
 	}
-	return 0;
+	cout << begin << "str:error" << endl;
+	return index;
 }
 
 //{fsff{ sfsdf{} }}";
 int for_blob(const char* begin) {
+	//cout << "len: " << strlen(begin) << endl;
 	int index = 0;
 	int start = -1;
 	while (char ch = *(begin + index)) {
@@ -41,20 +44,32 @@ int for_blob(const char* begin) {
 				start = index;
 		}
 		else if (ch == '}') {
+			if (start == -1) {
+				cout << begin << ":error" << endl;
+				return index;
+			}
 			return index;
 		}
 		else if (ch == '"') {
-			return for_key_value(begin + index);
+			if (start == -1) {
+				cout << begin << ":error" << endl;;
+				return index;
+			}
+			index += for_key_value(begin + index);
 		}
-		//else {
-		//	return parase(begin + index);
-		//}
+		else if(ch != ' '){
+			if (start == -1) {
+				cout << begin << ":error" << endl;;
+				return index;
+			}
+		}
 		index++;
 	}
 	return index;
 }
 
 int for_key_value(const char* begin) {
+	//cout <<"len"<< strlen(begin) << endl;
 	int index = 0;
 	int key_start = -1;
 	int key_end = -1;
@@ -68,27 +83,26 @@ int for_key_value(const char* begin) {
 				key_start = index;
 				index += for_str(begin + index);
 				key_end = index;
-				cout << string(begin + key_start, key_end - key_start + 1) << endl;
+				
 			}
 			else {
 				if (wait_val) {
 					val_start = index;
 					index += for_str(begin + index);
 					val_end = index;
-					cout << string(begin + val_start, val_end - val_start + 1) << endl;
+					cout << string(begin + key_start, key_end - key_start + 1) <<"  "<< string(begin + val_start, val_end - val_start + 1)<< endl;
+					//cout << string(begin + val_start, val_end - val_start + 1) << endl;
 					return index;
 				}
 			}
 		}
 		else if (wait_val && ch == '['){
-			return for_array(begin + index);
-			val_start = -1;
-			wait_val = false;
+			cout << string(begin + key_start, key_end - key_start + 1) << "   ";
+			return index + for_array(begin + index);
 		}
 		else if (wait_val && ch == '{') {
-			return for_blob(begin + index);
-			val_start = -1;
-			wait_val = false;
+			cout << string(begin + key_start, key_end - key_start + 1) << "   ";
+			return index + for_blob(begin + index);
 		}
 		else if (ch == ':') {
 			wait_val = true;
@@ -96,14 +110,20 @@ int for_key_value(const char* begin) {
 		else if (wait_val && val_start == -1 && ch != ' ') {
 			val_start = index;
 		}
-		else if (val_start != -1 && (ch == ' ' || ch == ',' || ch == '}' || ch == ']')) {
+		else if (val_start != -1 && (ch == ' ' || ch == ',' || ch == '}')) {
 			val_end = index;
-			cout << string(begin + val_start, val_end - val_start) << endl;
-			return index;
+			cout << string(begin + key_start, key_end - key_start + 1) << "  " << string(begin + val_start, val_end - val_start) << endl;
+			return index - 1;
 		}
 		index++;
 	}
-	return 0;
+	if (val_start != -1 && val_end == -1) {
+		val_end = index - 1;
+		cout << "k:" << string(begin + key_start, key_end - key_start + 1) << "  v:" << string(begin + val_start, val_end - val_start + 1) << endl;
+	}
+	else
+		cout << begin << ":error" << endl;
+	return index - 1;
 }
 
 
@@ -113,12 +133,12 @@ int parase(const char* begin) {
 		if (ch == '{') {
 			index+= for_blob(begin + index);
 		}
-		else if (ch == '"') {
-			index += for_key_value(begin + index);
-		}
-		else if (ch == '[') {
-			index += for_array(begin + index);
-		}
+		//else if (ch == '"') {
+		//	index += for_key_value(begin + index);
+		//}
+		//else if (ch == '[') {
+		//	index += for_array(begin + index);
+		//}
 		const char* cc = begin + index;
 		index++;
 	}
@@ -132,19 +152,34 @@ int for_array(const char* begin) {
 
 	int val_start = -1;
 
+	bool wait_val = true;
 	while (char ch = *(begin + index)) {
 		if (ch == ']') {
 			return index;
 		}
 		else if (ch == '[') {}
-		else if (ch == '{') {
-			return for_blob(begin + index);
+		else if (ch == '"') {
+			val_start = index;
+			int len = for_str(begin + index);
+			cout << string(begin + val_start, len + 1) <<" ";
+			index += len;
+			wait_val = false;
+			val_start = -1;
 		}
-		else if (val_start == -1 && ch != ' ' && ch != '\t') {
+		else if (ch == ',') {
+			wait_val = true;
+		}
+		else if (ch == '{') {
+			val_start = index;
+			index += for_blob(begin + index);
+			wait_val = false;
+			val_start = -1;
+		}
+		else if (wait_val && val_start == -1 && (ch != ' ' && ch != '\t')) {
 			val_start = index;
 		}
-		else if (val_start != -1 && ch == ' ' || ch == ',') {
-			cout << string(begin + val_start, index - val_start) << endl;
+		else if (val_start != -1 && (ch == ' ' || ch == ',' || ch == ']' || ch == '}')) {
+			cout << string(begin + val_start, index - val_start - 1) << " ";
 			val_start = -1;
 		}
 		index++;
@@ -201,9 +236,17 @@ int blob(const char* begin) {
 		else if (ch == ',') {
 			if (is_str == 0) {
 				val_end = index;
-				if (!is_blob || is_array) {
-					cout << "key:" << string(begin + key_begin + 1, key_end - key_begin - 1) << endl;
-					cout << "val:" << string(begin + val_begin + 1, val_end - val_begin - 1) << endl;
+				if (!is_blob) {
+					if (is_array) {
+						int i = for_array(next + 1);
+						//int i = blob(next + 1) + 1;
+						next += i;
+						index += i;
+					}
+					else {
+						cout << "key:" << string(begin + key_begin + 1, key_end - key_begin - 1) << endl;
+						cout << "val:" << string(begin + val_begin + 1, val_end - val_begin - 1) << endl;
+					}
 				}
 
 				if (is_array)
@@ -247,194 +290,7 @@ int blob(const char* begin) {
 	}
 }
 
-template<class T>
-struct date_imple_t {
-	typedef size_t(T::*unserialize_t)(const char*,size_t);
-	typedef void(T::*serialize_t)();
-	unserialize_t unserialize;
-	serialize_t serialize;
-};
-
-class colloect
-{
-public:
-	template<class T>
-	colloect(T *obj, const char* name, const date_imple_t<T> *field) {
-		obj->add_field(name, field);
-	};
-};
-
-template<class T>
-class json_base_t {
-public:
-	typedef T child_t;
-	void add_field(const char* name, const date_imple_t<T>* field) {
-		fields_[name] = field;
-	}
-	int blob(const char* begin) {
-		const char* next = begin;
-		int key_begin = -1;
-		int key_end = 0;
-		int val_begin = 0;
-		int val_end = 0;
-		int index = 0;
-		bool is_blob = false;
-		bool is_str = false;
-		bool is_array = false;
-		while (*next) {
-			char ch = *next;
-			if (ch == '{') {
-					if (is_str == 0) {
-						if (val_begin != 0) {
-							is_blob = true;
-							string key(begin + key_begin + 1, key_end - key_begin - 1);
-							int i = unserialize(key, begin + val_begin + 1, val_end - val_begin - 1) + 1;
-							next += i;
-							index += i;
-						}
-						else {
-							is_blob = true;
-							int i = blob(next + 1) + 1;
-							next += i;
-							index += i;
-						}
-					}
-			}
-			else if (ch == '[') {
-				is_array = true;
-				val_begin = index;
-			}
-			else if (ch == '"') {
-				is_blob = false;
-				if (0 == val_begin) {
-					if (key_begin != -1) {
-						key_end = index;
-					}
-					else
-						key_begin = index;
-				}
-				if (is_str)
-					is_str = false;
-				else
-					is_str = true;
-			}
-			else if (ch == ':') {
-				if (is_str == 0) {
-					val_begin = index;
-				}
-			}
-			else if (ch == ',') {
-				if (is_str == 0) {
-					val_end = index;
-					if (!is_blob || is_array) {
-						string key(begin + key_begin + 1, key_end - key_begin - 1);
-						unserialize(key, begin + val_begin + 1, val_end - val_begin - 1);
-					}
-					if (is_array)
-						val_begin = index;
-					else {
-						val_begin = 0;
-						key_begin = -1;
-					}
-				}
-			}
-			else if (ch == ']') {
-				if (is_str == 0) {
-					val_end = index;
-					if (!is_blob) {
-						string key(begin + key_begin + 1, key_end - key_begin - 1);
-						unserialize(key, begin + val_begin + 1, val_end - val_begin - 1);
-					}
-					is_array = false;
-
-					val_begin = 0;
-					key_begin = -1;
-				}
-			}
-			else if (ch == '}') {
-				if (is_str == 0) {
-					if (key_begin != -1) {
-						val_end = index;
-						if (!is_blob) {
-							string key(begin + key_begin + 1, key_end - key_begin - 1);
-							unserialize(key, begin + val_begin + 1, val_end - val_begin - 1);
-						}
-						val_begin = 0;
-						key_begin = -1;
-					}
-				}
-				return index;
-			}
-			next++;
-			index++;
-		}
-	}
-	size_t unserialize(string &key,const char* val,size_t len) {
-		auto iter = fields_.find(key);
-		if (iter != fields_.end()) {
-			return (((T*)this)->*(iter->second->unserialize))(val,len);
-		}
-		return 0;
-	}
-private:
-	static std::unordered_map<string, const date_imple_t<T>*> fields_;
-protected:
-	static T instance_;
-};
-template<class T>
-std::unordered_map<string, const date_imple_t<T>*> json_base_t<T>::fields_;
-template<class T>
-T json_base_t<T>::instance_;
-
-#include <sstream>
-class field_op {
-public:
-	static size_t us(int *data,const char* begin,size_t len){
-		string val(begin,len);
-		*data = stoi(val);
-		return 0;
-	}
-	static size_t us(double *data, const char* begin, size_t len) {
-		string val(begin, len);
-		*data = stod(val);
-		return 0;
-	}
-	static size_t us(string *data, const char* begin, size_t len) {
-		data->assign(begin + 1, len -2);
-		return 0;
-	}
-	static size_t us(float *data, const char* begin, size_t len) {
-		string val(begin, len);
-		*data = stof(val);
-		return 0;
-	}
-	template<class T>
-	static size_t us(vector<T> *data, const char* begin, size_t len) {
-		T v;
-		us(&v, begin, len);
-		data->push_back(v);
-		return 0;
-	}
-	template<class T>
-	static size_t us(json_base_t<T> *data, const char* begin, size_t len) {
-		return data->blob(begin);
-	}
-};
-
-#define N(name) \
-name;\
-private:\
-	size_t unserialize_##name( const char* begin,size_t len) {\
-		return field_op::us(&name,begin,len);\
-	}\
-	void serialize_##name() {\
-	}\
-	static constexpr date_imple_t<child_t> const_impl_##name = { &child_t::unserialize_##name,&child_t::serialize_##name };\
-	colloect collect__##name{this,#name,&const_impl_##name};\
-public:
-
-#define Json(name)\
-class name :public json_base_t<name>
+#include "xyz_json.h"
 
 Json(ff2) {
 public:
@@ -477,25 +333,103 @@ void perf_test(string name, size_t t, function<void()> f) {
 	endTime = clock();
 	cout << name << ":" << endTime - startTime << "ms" << endl;
 }
+#define R(...) #__VA_ARGS__
+
+void united_test() {
+	cout << "----------------for str-----------------" << endl;
+	cout << for_str("\"sdfdsf\"") << endl;
+
+	//const char* str = "\"a\":\"sfsdf\"";
+	cout << "----------------for key_value-----------------" << endl;
+	cout << for_key_value("\"a\":\"sfsdf\"") << endl;
+	cout << for_key_value("\"a\":12321") << endl;
+	cout << for_key_value(" \"a{}[]  \":  12321") << endl;
+
+	cout << "----------------for blob-----------------" << endl;
+	cout << " " << for_blob("{\"a\":\"sfsdf\"}") << endl;
+	cout << " " << for_blob("\"a\":12321") << endl;
+	cout << " " << for_blob(" \"a{}[]  \":  12321") << endl;
+	cout << " " << for_blob("{\"a\":12321}") << endl;
+	cout << " " << for_blob(" { \"a{}[]  \":  12321}") << endl;
+	cout << " " << for_blob("{\"employee\":{ \"name\":\"Bill Gates\", \"age\" : 62, \"city\" : \"Seattle\" },\"job\":\"seal\"}") << endl;
+	cout << " " << for_blob("{\"employee\":{ \"name\":\"Bill Gates\", \"age\" : 62, \"city\" : \"Seattle\" },\"job\":[\"seal\",\"sdf\"]}") << endl;
+
+	cout << "----------------for array-----------------" << endl;
+	cout << " " << for_array("[123,123,123]");
+	cout << " " << for_array("[\"aa\",\"bb\",\"cc\"]");
+	cout << " " << for_array("[{\"a\":\"sfsdf\"},{\"a\":\"sfsfdsfdf\"]");
+	cout << " " << for_array(R([{ "name":"Porsche", "models" : ["911", "Taycan"] },
+		{ "name":"BMW", "models" : ["M5", "M3", "X5"] },
+		{ "name":"Volvo", "models" : ["XC60", "V60"] }])) << endl;
+
+
+	for_blob(R(
+		{
+			"employees": [
+	{
+		"firstName": "Bill",
+			"lastName" : "Gates"
+	},
+		{
+			"firstName": "George",
+			"lastName" : "Bush"
+		},
+		{
+			"firstName": "Thomas",
+			"lastName" : "Carter"
+		}
+			]
+		}
+	));
+}
+
+void serialize_test() {
+
+}
+
+void complex_test() {
+
+}
+
+Json(HeWeather6)
+{
+	Json(Basic) {
+		std::string N(cid);// : "CN101010100",
+		std::string	N(location);// " : "北京",
+		std::string	N(parent_city);// : "北京",
+		std::string	N(admin_area);// : "北京",
+		std::string	N(cnty);// : "中国",
+		std::string	N(lat);// : "39.90498734",
+		std::string	N(lon);// : "116.40528870",
+		std::string	N(tz);// : "8.0"
+		int N(ca);
+	};
+	Basic N(basic);
+	vector<Basic> N(basics);
+};
+
+#include "json.hpp"
+
+Json(OO1)
+{
+public:
+	string N(firstName);
+	string N(lastName);
+};
+
+Json(OO)
+{
+public:
+	vector<OO1> N(employees);
+};
 
 int main()
 {
-	//const char* str1 = "\"sfsdf\"";
-	//cout << for_str(str1) << endl;
-
-	//const char* str2 = "{fsff{sfsdf{}}}";
-	//for_blob(str2);
-
-	cout << "key_val" << endl;
-	const char* str3 = "{   \"yes\" : \"hello\",\"hh\":123 }";
-	parase(str3);
-	//parase(str3);
-	//getchar();
-	//const char* jo = "{  \"a\":1,\"b\":2,\"e\":{\"a\":1,\"b\":2},\"c\":\"sfsf\",\"d\":3,\"f\":[1,2,3]}";
-	//blob(jo);
-
-	//ff kkl;
-	//kkl.blob(jo);
+	string res;
+	HeWeather6 ffff;
+	ffff.serialize(res);
+	cout << res << endl;
+	for_blob(res.data());
 	ifstream myfile("hello.txt");
 	string jj;
 	string temp;
@@ -513,19 +447,94 @@ int main()
 	//	for (int i = 0; i < 100000; i++)
 	//		parase(jj.data());
 	//});
-	parase(jj.data());
+	jj += "}";
+	nlohmann::json j_patch = R"(		{
+			"employees": [
+	{
+		"firstName": "Bill",
+		"lastName" : "Gates"
+	},
+	{
+		"firstName": "George",
+		"lastName" : "Bush"
+	},
+	{
+		"firstName": "Thomas",
+		"lastName" : "Carter"
+	}
+			]
+		}
+	)"_json;
+	//cout<<j_patch["employees"][0]["firstName"].get<string>();
+	//nlohmann::json fsf;
+	//fsf.parse(jj.data(), jj.data() + jj.size());
+	//cout << fsf["name"].get<std::string>() << endl;
+
+
 	//perf_test("table build 1", 1, [&jj]()->void {
-	//	for (int i = 0; i < 100000; i++)
-	//		blob(jj.data());
+	//	for (int i = 0; i < 1000000; i++) {
+	//		nlohmann::json j_patch = R"(		{
+	//		"employees": [
+	//{
+	//	"firstName": "Bill",
+	//	"lastName" : "Gates"
+	//},
+	//{
+	//	"firstName": "George",
+	//	"lastName" : "Bush"
+	//},
+	//{
+	//	"firstName": "Thomas",
+	//	"lastName" : "Carter"
+	//}
+	//		]
+	//	}
+	//)"_json;
+	//		j_patch["employees"][0]["firstName"].get<string>();
+	//	}
 	//});
-	blob(jj.data());
-	parase(jj.data());
 
-	const char *json = "{\"employee\":{ \"name\":\"Bill Gates\", \"age\" : 62, \"city\" : \"Seattle\" },\"job\":\"seal\"}";
-	blob(jj.data());
-	NN nn;
-	nn.blob(jj.data());
 
+	//perf_test("table build 1", 1, [&jj]()->void {
+	//	for (int i = 0; i < 1000000; i++) {
+	//		OO nn;
+	//		nn.for_blob(R({
+	//			"employees": [
+	//		{
+	//			"firstName": "Bill",
+	//				"lastName" : "Gates"
+	//		},
+	//{
+	//	"firstName": "George",
+	//	"lastName" : "Bush"
+	//},
+	//{
+	//	"firstName": "Thomas",
+	//	"lastName" : "Carter"
+	//}
+	//			]
+	//			}));
+	//		nn.employees[0].firstName;
+	//		
+	//	}
+	//});
+	//NN nn;
+	//nn.for_blob(jj.data());
+
+
+	HeWeather6 f;
+	f.for_blob(R(
+	{
+		"basic": {
+			"cid": "CN101010100",
+				"location" : "北京",
+				"parent_city" : "北京",
+				"admin_area" : "北京",
+				"cnty" : "中国",
+				"lat" : "39.90498734",
+				"lon" : "116.40528870",
+				"tz" : "8.0"
+		}}));
     std::cout << "Hello World!\n"; 
 	getchar();
 }
